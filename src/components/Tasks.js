0a2b3a53d32be12a88ec0cbbebb22492270fe8bf
Dev Bachani <dev22603@gmail.com>
2023-06-26 14:30:42 +0530

    Automated the display of todos in Tasks.js by the map method

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,18 +5,9 @@ import TaskItem from "./TaskItem";
 const Tasks = (props) => {
   return (
     <Card className="TaskBox">
-      <TaskItem
-        taskName={props.todoList[0].name}
-        taskDuration={props.todoList[0].duration}
-      ></TaskItem>
-      <TaskItem
-        taskName={props.todoList[1].name}
-        taskDuration={props.todoList[1].duration}
-      ></TaskItem>
-      <TaskItem
-        taskName={props.todoList[2].name}
-        taskDuration={props.todoList[2].duration}
-      ></TaskItem>
+      {props.todoList.map((todo) => (
+        <TaskItem taskName={todo.name} taskDuration={todo.duration} />
+      ))}
     </Card>
   );
 };
